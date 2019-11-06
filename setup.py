d26fac29bf81344733aa474e19c21b3134288f2a
Stephen O'Shea <stephen.oshea@beboc.co.uk>
2019-11-06 16:29:41 +0000

    Markdown

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
   version = 'v0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'neuralnetwork',   # Give a short description about your library
-  long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Stephen OShea',                   # Type in your name
   author_email = 'stephenlmoshea@gmail.com',      # Type in your E-Mail
