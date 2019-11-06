3735babb7f5dbb985f2cbafd354be182ae25b91b
Stephen O'Shea <stephen.oshea@beboc.co.uk>
2019-11-06 11:53:40 +0000

    removing requires

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from distutils.core import setup
 setup(
   name = 'neuralnetwork',         # How you named your package folder (MyLib)
   packages = ['neuralnetwork'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'neuralnetwork',   # Give a short description about your library
   author = 'Stephen OShea',                   # Type in your name
   author_email = 'stephenlmoshea@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/stephenlmoshea/python-neuralnetwork',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/stephenlmoshea/python-neuralnetwork/archive/02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/stephenlmoshea/python-neuralnetwork/archive/0.2.tar.gz',    # I explain this later on
   keywords = ['NEURAL NETWORK', 'BACKPROPAGATION', 'GRADIENT DESCENT', 'SIGMOID', 'HYPERBOLIC TANGENT'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-         'random',
-         'pickle'
+        
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
