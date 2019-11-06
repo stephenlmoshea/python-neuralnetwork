33d19dc9baed6101e522198e1563a6dff057d76c
Stephen O'Shea <stephen.oshea@beboc.co.uk>
2019-11-06 11:18:56 +0000

    moving files into folder

--- HyperbolicTangent.py
+++
@@ -1,8 +0,0 @@
-import math
-
-class HyperbolicTangent:
-    def getActivation(self,net):
-        return math.tanh(net)
-
-    def getDerivative(self,net):
-        return 1 - (self.getActivation(net) * self.getActivation(net))
