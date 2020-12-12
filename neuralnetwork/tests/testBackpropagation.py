962632310be6bf96015de9684518f5493679d3dd
Stephen O'Shea <stephenlmoshea@gmail.com>
2020-12-12 04:00:27 +0000

    Adding tests

--- neuralnetwork/tests/testBackpropagation.py
+++ neuralnetwork/tests/testBackpropagation.py
@@ -160,3 +160,85 @@ class TestBackpropagation(unittest.TestCase):
         feedForward.activate([1,1])
         outputs = feedForward.getOutputs()
         self.assertTrue(outputs[0] < 0.1)
+
+    def testItLearnsXOrFunctionWithFourHiddenUnits(self):
+        sigmoid = Sigmoid()
+
+        networkLayer = [2,4,1]
+
+        feedForward = FeedForward(networkLayer, sigmoid)
+
+        backpropagation = Backpropagation(feedForward,0.7,0.3)
+
+        trainingSet = [
+            [0,0,0],
+            [0,1,1],
+            [1,0,1],
+            [1,1,0]
+        ]
+
+        while True:
+            backpropagation.initialise()
+            result = backpropagation.train(trainingSet)
+
+            if(result):
+                break
+
+        feedForward.activate([0,0])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] < 0.1)
+
+        feedForward.activate([0,1])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] > 0.9)
+
+        feedForward.activate([1,0])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] > 0.9)
+
+        feedForward.activate([1,1])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] < 0.1)
+
+    def testItLearnsXOrFunctionWithThreeInputNodes(self):
+        sigmoid = Sigmoid()
+
+        networkLayer = [3,4,1]
+
+        feedForward = FeedForward(networkLayer, sigmoid)
+
+        backpropagation = Backpropagation(feedForward,0.7,0.3,0.0005, 5000)
+
+        trainingSet = [
+            [0,0,0,0],
+            [0,0,1,1],
+            [0,1,0,1],
+            [0,1,1,0],
+            [1,0,0,1],
+            [1,0,1,0],
+            [1,1,0,0],
+            [1,1,1,1]
+        ]
+
+        while True:
+            backpropagation.initialise()
+            result = backpropagation.train(trainingSet)
+
+            if(result):
+                break
+
+        feedForward.activate([0,0,0])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] < 0.1)
+
+        feedForward.activate([0,1,0])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] > 0.9)
+
+        feedForward.activate([1,1,1])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] > 0.9)
+
+        feedForward.activate([1,1,0])
+        outputs = feedForward.getOutputs()
+        self.assertTrue(outputs[0] < 0.1)
